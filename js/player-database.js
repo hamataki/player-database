bddfcea0db30724b23562d7a7df566869c579c7e
hamataki <hamatakiryunosuke@gmail.com>
2024-05-24 02:06:20 +0900

    hoverの追加

--- js/player-database.js
+++ js/player-database.js
@@ -73,7 +73,7 @@ for (let i = 0; i < 13; i++) {
 
   //名前，国，チーム，生年月日
   const html = `
-    <div class="mx-1 my-4 p-5 border-2 rounded-3xl shadow-md grid-cols-3 md:w-5/12">
+    <div class="mx-1 my-4 p-5 border-2 rounded-3xl shadow-md grid-cols-3 md:w-5/12 hover:scale-105">
       <li class="p-5">
         <p id="name-${i}">${"Name:" + nam}</p>
         <p>${"Country:" + con}</p>
@@ -93,10 +93,10 @@ for (let i = 0; i < 13; i++) {
         placeholder="本文を入力"
         class="justify-center items-center p-5 w-full"></textarea>
       <div class="flex justify-end m-2">
-      <div class="p-2">
+      <div class="p-2 hover:-translate-y-1 hover:scale-110">
         <button class="save-btn" data-index="${i}">セーブ</button>
       </div>
-      <div class="p-2">
+      <div class="p-2 hover:-translate-y-1 hover:scale-110">
         <button class="clear-btn" data-index="${i}">削除</button>
       </div>
       </div>
@@ -142,68 +142,16 @@ $(document).on("click", ".clear-btn", function () {
 for (let i = 0; i < localStorage.length; i++) {
   const key = localStorage.key(i);
   const value = localStorage.getItem(key);
-  const html2 =
-    `
-    <div class="border-2 scroll-m-1 rounded-3xl p-5 w-2/5 shadow-lg grid-cols-3">
+  const html2 = `
+    <div class="border-2 scroll-m-1 rounded-3xl p-5 w-2/5 shadow-lg grid-cols-3 hover:scale-105">
     <li class="p-2">
     <p>${key}</p>
     <P>${value}</p>
     </li>
     </div>
-    `
+    `;
   $("#memo").append(html2);
 }
-
-
-// // Save クリックイベント
-// $(document).on("click", ".save-btn", function () {
-//   const index = $(this).data("index");
-//   const nam = $("#name-" + index).text();
-//   const key = $("#title-" + index).val();
-//   const value = $("#text-" + index).val();
-//   if (key && value) {
-//     localStorage.setItem(nam, key, value);
-//     $("#memo").append(
-//       `
-//     <div class="border-2 scroll-m-1 rounded-3xl p-5 w-2/5 shadow-lg grid-cols-3">
-//     <li class="p-2">
-//     <p>${nam + "選手"}</p>
-//     <P>${key}: ${value}</p>
-//     </li>
-//     </div>
-//     `
-//     );
-//   } else {
-//     alert("タイトルと本文を入力してください。");
-//   }
-// });
-
-// // Clear クリックイベント
-// $(document).on("click", ".clear-btn", function () {
-//   const index = $(this).data("index");
-//   const nam = $("#name-" + index).text();
-//   const key = $("#title-" + index).val();
-//   const value = $("#text-" + index).val();
-
-//   localStorage.removeItem(nam, key, value);
-//   location.reload(); // ページをリロードして表示を更新
-// });
-
-// // ページ読み込み：保存データ取得表示
-// for (let i = 0; i < localStorage.length; i++) {
-//   const key = localStorage.key(i);
-//   const value = localStorage.getItem(key);
-//   const html2 =
-//     `
-//     <div class="border-2 scroll-m-1 rounded-3xl p-5 w-2/5 shadow-lg grid-cols-3">
-//     <li class="p-2">
-//     <p>${key}</p>
-//     <P>${value}</p>
-//     </li>
-//     </div>
-//     `
-//   $("#memo").append(html2);
-// }
 
 // 全てClear クリックイベント
 $("#clear").on("click", function () {
